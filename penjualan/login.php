e63feaad13e0de78effa0a62e8579e36fd5f8a37
RintaMartha <114599568+RintaMartha@users.noreply.github.com>
2023-09-27 07:47:29 +0700

    Add files via upload

---
+++ penjualan/login.php
@@ -0,0 +1,45 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEBSITE</title>
+    <link rel="stylesheet" type="text/css" href="style/stylesheet.css">
+</head>
+<body>
+
+    <br>
+    <br>
+    <div align="center">
+        <img src="img/smk9.png" width="100" height="120">
+        <img src="img/mart.png" width="190" height="170">
+    </div>
+        <br>
+        <br>
+        &nbsp;
+       
+            <div class="table">
+                    <h3 style="text-align: center;color:black">APLIKASI "NINE MART"</h3>
+                    <center>
+                    <h2>LOGIN</h2>
+                    
+                    <form action="proses_login.php" method="post">
+                        <b>
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" required><br><br>
+
+                        <label for="alamat">Alamat:</label>
+                        <input type="alamat" name="alamat" required><br><br>
+                        </b>
+                        <input type="submit" value="LOGIN">
+                        &nbsp;
+                    </form>
+
+<footer>
+    <div style="color: rgb(89, 0, 255)" align="center"><strong> Copyright &copy; 2023 SMK NEGERI 9 SEMARANG</strong></div>
+</footer>
+                    <center>
+            </div>
+</body>
+</html>
